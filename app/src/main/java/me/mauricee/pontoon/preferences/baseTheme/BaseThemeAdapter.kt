462d55c1fbbbea35c8f4e4bb794915da89f760b6
Maurice <medwar40@gmail.com>
2018-09-11 07:37:53 -0400

    Cleaned up design of settings

--- app/src/main/java/me/mauricee/pontoon/preferences/baseTheme/BaseThemeAdapter.kt
+++ app/src/main/java/me/mauricee/pontoon/preferences/baseTheme/BaseThemeAdapter.kt
@@ -1,6 +1,5 @@
 package me.mauricee.pontoon.preferences.baseTheme
 
-import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,34 +7,60 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding2.view.clicks
 import io.reactivex.rxkotlin.plusAssign
-import kotlinx.android.synthetic.main.item_color.view.*
+import kotlinx.android.synthetic.main.item_color_list.view.*
 import me.mauricee.pontoon.R
 import me.mauricee.pontoon.common.BaseAdapter
 import me.mauricee.pontoon.common.theme.BaseTheme
+import me.mauricee.pontoon.common.theme.ThemeManager
+import me.mauricee.pontoon.preferences.CircleHelper
 import javax.inject.Inject
 
-class BaseThemeAdapter @Inject constructor() : BaseAdapter<BaseTheme, BaseThemeAdapter.ViewHolder>() {
-    private val colors = BaseTheme.values()
+class BaseThemeAdapter @Inject constructor(private val circleHelper: CircleHelper, private val themeManager: ThemeManager) : BaseAdapter<BaseTheme, BaseThemeAdapter.ViewHolder>() {
+    private val themes = BaseTheme.values()
+    private var newSelectedTheme: BaseTheme? = null
+
+    init {
+        setHasStableIds(true)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_color, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_color_grid, parent, false)
                     .let(this::ViewHolder)
 
-    override fun getItemCount(): Int = colors.size
+    override fun getItemId(position: Int): Long = themes[position].ordinal.toLong()
+
+    override fun getItemCount(): Int = themes.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val theme = colors[position]
-        val color = when (theme) {
+        val isOriginal = themeManager.baseTheme == themes[position]
+        val isChecked = newSelectedTheme == themes[position]
+        val context = holder.itemView.context
+        val color = when (themes[position]) {
             BaseTheme.Light -> R.color.white
             BaseTheme.Dark -> R.color.md_grey_850
             BaseTheme.Black -> R.color.md_black_1000
+        }.let { ContextCompat.getColor(context, it) }
+
+        val drawable = when {
+            isOriginal -> circleHelper.buildSelectedCircle(color, ContextCompat.getColor(context, R.color.colorThemeSelection))
+            isChecked -> circleHelper.buildSelectedCircle(color, ContextCompat.getColor(context, R.color.md_green_A400))
+            else -> circleHelper.buildCircle(color)
         }
-        holder.itemView.color.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(holder.itemView.context, color))
+        holder.itemView.color.setImageDrawable(drawable)
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         init {
-            subscriptions += itemView.clicks().map { colors[layoutPosition] }.subscribe(relay::accept)
+            subscriptions += itemView.clicks().map { themes[layoutPosition] }
+                    .subscribe {
+                        newSelectedTheme?.let {
+                            notifyItemChanged(themes.indexOf(it))
+                        }
+                        newSelectedTheme = it
+                        relay.accept(it)
+                        notifyItemChanged(layoutPosition)
+                    }
         }
     }
+
 }
