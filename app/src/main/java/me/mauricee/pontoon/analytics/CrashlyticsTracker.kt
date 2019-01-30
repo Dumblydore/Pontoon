14eb4f2f71065a55311680caeee3ec2dc7d5c294
Maurice Edwards <medwar40@gmail.com>
2019-01-30 17:02:48 -0500

    Removed crashlytics listener stuff

--- app/src/main/java/me/mauricee/pontoon/analytics/CrashlyticsTracker.kt
+++ app/src/main/java/me/mauricee/pontoon/analytics/CrashlyticsTracker.kt
@@ -8,11 +8,12 @@ class CrashlyticsTracker @Inject constructor(): EventTracker.Tracker {
     private val actionStateQueue: Queue<String> = ArrayDeque()
 
     init {
-        val handler = Thread.getDefaultUncaughtExceptionHandler()
-        Thread.setDefaultUncaughtExceptionHandler { paramThread, paramThrowable ->
-            Crashlytics.log(actionStateQueue.reduce { i, i2 -> i + "$i2\n" })
-            handler.uncaughtException(paramThread, paramThrowable)
-        }
+        //TODO figure out how to push up to capture crashlytics exception handler
+//        val handler = Thread.getDefaultUncaughtExceptionHandler()
+//        Thread.setDefaultUncaughtExceptionHandler { paramThread, paramThrowable ->
+//            Crashlytics.log(actionStateQueue.reduce { i, i2 -> i + "$i2\n" })
+//            handler.uncaughtException(paramThread, paramThrowable)
+//        }
     }
 
     override fun trackAction(action: EventTracker.Action, page: EventTracker.Page) {
